07b2f9a6c26172a76db36adec9551c7bb6953063
Ajit <patilajit2206@gmail.com>
2020-10-03 15:44:57 +0530

    commit 2

--- converters.py
+++ converters.py
@@ -1,8 +1,8 @@
 def lbs_to_kg(weight):
-    return weighr*0.45
+    return weight*0.45
     print(weight)
 
 def kg_to_lbs(weight):
     return weight/0.45
     print(weight)
-    print("\n weight is printed")
+    print("\n weight is printed very good")
